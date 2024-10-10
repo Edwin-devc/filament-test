064215c586b4a720fe1ef10c5c85b2b2213cc129
Rwakasiisi Edwin <erwakasiisi@gmail.com>
2024-10-10 08:55:45 +0300

    final commit

---
+++ app/Casts/MoneyCast.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class MoneyCast implements CastsAttributes
+{
+    public function get($model, string $key, $value, array $attributes): float
+{
+    return round(floatval($value) / 100, precision: 2);
+}
+ 
+public function set($model, string $key, $value, array $attributes): float
+{
+    return round(floatval($value) * 100);
+}
+}
